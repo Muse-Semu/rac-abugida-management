dd371c0ac1166332d83fb6053aeef8fb50d58781
mama <muses@act.com.et>
2025-05-05 13:56:44 +0300

    store for component

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
 import eventReducer from './slices/eventSlice';
 import projectReducer from './slices/projectSlice';
+import userReducer from './slices/userSlice';
 import dashboardReducer from './slices/dashboardSlice';
 
 export const store = configureStore({
@@ -9,6 +10,7 @@ export const store = configureStore({
     auth: authReducer,
     events: eventReducer,
     projects: projectReducer,
+    users: userReducer,
     dashboard: dashboardReducer,
   },
 });
