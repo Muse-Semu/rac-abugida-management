d8786787d18280d3e19dd5c88aed7a39b7e459e8
mama <smuse1756@gmail.com>
2025-05-17 16:45:01 +0300

    update indes store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './slices/authSlice';
-import eventReducer from './slices/eventSlice';
-import projectReducer from './slices/projectSlice';
 import userReducer from './slices/userSlice';
+import projectReducer from './slices/projectSlice';
+import eventReducer from './slices/eventSlice';
+import authReducer from './slices/authSlice';
 import dashboardReducer from './slices/dashboardSlice';
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer,
-    events: eventReducer,
-    projects: projectReducer,
     users: userReducer,
+    projects: projectReducer,
+    events: eventReducer,
+    auth: authReducer,
     dashboard: dashboardReducer,
   },
 });
