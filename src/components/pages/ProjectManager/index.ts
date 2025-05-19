849657dfee8d62791abece94eacc5871ad4a3f77
mama <smuse1756@gmail.com>
2025-05-19 15:19:21 +0300

    Make index for export project components

---
+++ src/components/pages/ProjectManager/index.ts
@@ -0,0 +1,5 @@
+export { default as ProjectList } from "./ProjectList";
+export { default as ProjectCard } from "./ProjectCard";
+export { default as ProjectDetails } from "./ProjectDetails";
+export { default as ProjectForm } from "./ProjectForm";
+export { default as DeleteConfirmation } from "./DeleteConfirmation";
